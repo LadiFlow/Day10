cacf305d4fa6fa7feabcae816f70ba5c5bfc5058
LadiFlow <adi_curt@hotmail.com>
2021-03-17 20:05:44 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myFunction(myObj) {
+return myObj;
+}
+var myPet = {
+    species:"Dog",
+    name:"cura",
+    legs:4,
+    friends:["bruca", "brada"],
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
