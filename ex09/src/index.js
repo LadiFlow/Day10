2e0632b3d9ed28f418546ce10a044aaec060c5cf
LadiFlow <adi_curt@hotmail.com>
2021-03-18 00:40:17 +0100

    .hasOwnProperty method

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function myFunction(myObj, checkProp) {
+// Only change code below this line
+var myObj = {
+title: "Titanic",
+song: "My Heart Will Go On",
+genre: "drama"
+
+}
+
+
+
+return myObj[checkProp];
+
+
+// Only change code above this line
+}
+module.exports = myFunction;
