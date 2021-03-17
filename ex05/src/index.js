66db44d60aef42e1c827d7c12bf8323dc6650b05
LadiFlow <adi_curt@hotmail.com>
2021-03-17 21:44:00 +0100

    Updating object properties

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+var school = {
+name:"Arena",
+location:"Sarajevo",
+established:"2020"
+}
+
+function myFunction(name) {
+school.name = "Paragon";
+return school;
+}
+console.log(myFunction("Paragon"));
+module.exports = { school, myFunction};
