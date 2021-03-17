9542c554dfb69de31a7e75fad52d81efdf0fddab
LadiFlow <adi_curt@hotmail.com>
2021-03-17 22:08:20 +0100

    Delete properties from an object

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+var lion = {
+    name:"Simba",
+    legs:4,
+    tails:1,
+    roar:"roar-roar"
+}
+function myFunction(roar){
+   delete lion[roar];
+    return lion;
+}
+console.log(myFunction("roar"));
+module.exports = myFunction;
